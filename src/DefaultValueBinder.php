cc53678fad258411b39314c3dcbb3bd2be942480
CHAMNAB <chamnab.roeun.rc@gmail.com>
2025-05-01 11:16:13 +0700

    Update DefaultValueBinder.php

--- src/DefaultValueBinder.php
+++ src/DefaultValueBinder.php
@@ -7,11 +7,6 @@ use PhpOffice\PhpSpreadsheet\Cell\DefaultValueBinder as PhpSpreadsheetDefaultVal
 
 class DefaultValueBinder extends PhpSpreadsheetDefaultValueBinder
 {
-    /**
-     * @param  Cell  $cell  Cell to bind value to
-     * @param  mixed  $value  Value to bind in cell
-     * @return bool
-     */
     public function bindValue(Cell $cell, $value)
     {
         if (is_array($value)) {
